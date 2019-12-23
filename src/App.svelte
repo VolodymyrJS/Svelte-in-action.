f2e4657d8d1861c6326cec7604ea8f5e8323f88e
Volodymyr Ponomarenko <volodymyr.ponomarenko@extrawest.com>
2019-12-23 15:17:41 +0200

    split up on components and passing data acros components

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,89 @@
 <script>
-    import Header from './MeetUps/Header.svelte';
-    import Footer from './MeetUps/Footer.svelte';
+  import Header from "./UI/Header.svelte";
+  import MeetupGrid from "./Meetups/MeetupGrid.svelte";
+
+  let meetups = [
+    {
+      id: "m1",
+      title: "Coding Bootcamp",
+      subtitle: "Learn to code in 2 hours",
+      description:
+        "In this meetup, we will have some experts that teach you how to code!",
+      imageUrl:
+        "https://blog.vilmatech.com/wp-content/uploads/2019/05/svelte.jpg",
+      address: "27th Nerd Road, 32523 New York",
+      contactEmail: "code@test.com"
+    },
+    {
+      id: "m2",
+      title: "Swim Together",
+      subtitle: "Let's go for some swimming",
+      description: "We will simply swim some rounds!",
+      imageUrl:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Olympic_swimming_pool_%28Tbilisi%29.jpg/800px-Olympic_swimming_pool_%28Tbilisi%29.jpg",
+      address: "27th Nerd Road, 32523 New York",
+      contactEmail: "swim@test.com"
+    }
+  ];
+
+  let title = '';
+  let subtitle = '';
+  let description = '';
+  let imageUrl = '';
+  let address = '';
+  let email = '';
+
+  function addMeetup() {
+    console.log('addMeetup was clicked');
+    const newMeetup = {
+        id: Math.random(),
+        title: title,
+        subtitle: subtitle,
+        description: description,
+        imageUrl: imageUrl,
+        address: address,
+        contactEmail: email
+    };
+    meetups = [...meetups, newMeetup];
+  }
+
 </script>
 
 <style>
-
-    .Site {
-        display: flex;
-        min-height: 100vh;
-        flex-direction: column;
-    }
-
-    .site-content {
-        align-self: center;
-        margin-top: 80px;
-        flex: 1;
-        justify-items: center;
-        justify-content: center;
-    }
-
+  main {
+    margin-top: 5rem;
+  }
 </style>
 
-<body class="Site">
-    <Header />
-    <div class="site-content">Main content</div>
-    <Footer />
-</body>
+<Header />
 
-
+<main>
+  <form on:submit|preventDefault={addMeetup}>
+    <div class="form-group">
+        <label for="title">Title</label>
+        <input type="text" bind:value={title} />
+    </div>
+    <div class="form-group">
+        <label for="title">Subtitle</label>
+        <input type="text" bind:value={subtitle} />
+    </div>
+    <div class="form-group">
+        <label for="title">Description</label>
+        <textarea type="text" bind:value={description}></textarea>
+    </div>
+    <div class="form-group">
+        <label for="title">ImageUrl</label>
+        <input type="text" bind:value={imageUrl} />
+    </div>
+    <div class="form-group">
+        <label for="title">Address</label>
+        <input type="text" bind:value={address} />
+    </div>
+    <div class="form-group">
+        <label for="title">Email</label>
+        <input type="email" bind:value={email} />
+    </div>
+    <button type="submit">Add meetup</button>
+  </form>
+  <MeetupGrid {meetups} />
+</main>
---
+++ src/UI/Header.svelte
@@ -0,0 +1,28 @@
+<style>
+  header {
+    position: fixed;
+    width: 100%;
+    top: 0;
+    left: 0;
+    height: 4rem;
+    background: lightslategray;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    box-shadow: 0 2px 6px rgba(0, 0, 0, 0.26);
+  }
+
+  h1 {
+    color: lightskyblue;
+    font-family: "Roboto Slab", serif;
+    margin: 0;
+    text-shadow: 
+        0px 4px 3px rgba(0,0,0,0.4),
+        0px 8px 13px rgba(0,0,0,0.1),
+        0px 18px 23px rgba(0,0,0,0.1);
+  }
+</style>
+
+<header>
+  <h1>Extrawest Meetups</h1>
+</header>
